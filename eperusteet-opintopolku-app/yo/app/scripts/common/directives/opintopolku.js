d111e91f734a76570a7298f86bf923877efd296f
Antti Pekka Vilkko <antti.pekka.vilkko@gofore.com>
2015-03-06 15:35:12 +0200

    Oma header-raami opintopolkulinkille

---
+++ eperusteet-opintopolku-app/yo/app/scripts/common/directives/opintopolku.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software: Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * European Union Public Licence for more details.
+ */
+
+'use strict';
+
+epOpintopolkuApp
+.directive('opintopolkuHeader', function () {
+  return {
+    scope: {},
+    restrict: 'E',
+    templateUrl: 'views/common/directives/opintopolkuheader.html',
+    controller: function ($scope, Kieli) {
+      $scope.kieli = Kieli.getUiKieli();
+      $scope.other = $scope.kieli === 'fi' ? 'sv' : 'fi';
+      $scope.urls = {
+        eperusteet: {
+          fi: {
+            url: 'https://eperusteet.opintopolku.fi/',
+            label: 'ePerusteet',
+          },
+          sv: {
+            url: 'https://egrunder.studieinfo.fi/',
+            label: 'eGrunder'
+          }
+        },
+        opintopolku: {
+          fi: {
+            url: 'https://opintopolku.fi/',
+            label: 'Opintopolku'
+          },
+          sv: {
+            url: 'https://studieinfo.fi/',
+            label: 'Studieinfo'
+          }
+        }
+      };
+      $scope.kieliLabel = $scope.kieli === 'fi' ? 'På svenska' : 'Suomeksi';
+    }
+  };
+});
