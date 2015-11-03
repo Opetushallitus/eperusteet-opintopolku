fd19c98732b26b45cf06b8e1454155bbaf0ffbfe
Sauli Anto <sauli.anto@gofore.com>
2015-11-03 16:43:12 +0200

    Tutkinnon muodostumisen näkymän puurakenne muutettu responsiiviseksi

--- eperusteet-opintopolku-app/yo/app/scripts/common/directives/sulje-valikko.js
+++ eperusteet-opintopolku-app/yo/app/scripts/common/directives/sulje-valikko.js
@@ -1,12 +1,19 @@
+'use strict';
+
 epOpintopolkuApp
   .directive('offClick', ['$document', '$timeout', function ($document, $timeout) {
 
   function targetInFilter(target, filter) {
-    if (!target || !filter) return false;
+    if (!target || !filter) {
+      return false;
+    }
     var elms = angular.element(document.querySelectorAll(filter));
     var elmsLen = elms.length;
-    for (var i = 0; i < elmsLen; ++i)
-      if (elms[i].contains(target)) return true;
+    for (var i = 0; i < elmsLen; ++i) {
+      if (elms[i].contains(target)) {
+        return true;
+      }
+    }
     return false;
   }
 
@@ -17,7 +24,17 @@ epOpintopolkuApp
       offClickIf: '&'
     },
     link: function (scope, elm, attr) {
+      function handler(event) {
+        if (event.pageX === 0 && event.pageY === 0) {
+          return;
+        }
 
+        var target = event.target || event.srcElement;
+        if (!(elm[0].contains(target) || targetInFilter(target, attr.offClickFilter))) {
+          scope.$apply(scope.offClick());
+        }
+      }
+      
       if (attr.offClickIf) {
         scope.$watch(scope.offClickIf, function (newVal, oldVal) {
             if (newVal && !oldVal) {
@@ -36,15 +53,6 @@ epOpintopolkuApp
       scope.$on('$destroy', function () {
         $document.off('click', handler);
       });
-
-      function handler(event) {
-        if (event.pageX == 0 && event.pageY == 0) return;
-
-        var target = event.target || event.srcElement;
-        if (!(elm[0].contains(target) || targetInFilter(target, attr.offClickFilter))) {
-          scope.$apply(scope.offClick());
-        }
-      }
     }
   };
 }]);
