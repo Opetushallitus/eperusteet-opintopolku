aad8fd8c3d863bd7c4b63bcd3993583ccef18b76
Richard Van Camp <richard.vancamp@gofore.com>
2015-11-13 10:09:17 +0200

    removed unneeded variables

--- eperusteet-opintopolku-app/yo/app/eperusteet-esitys/views/lukio.js
+++ eperusteet-opintopolku-app/yo/app/eperusteet-esitys/views/lukio.js
@@ -43,9 +43,7 @@ angular.module('eperusteet.esitys')
     $scope.perusteenSisalto = perusData;
     $scope.peruste = peruste;
     $scope.oppiaineet = _.zipBy($scope.oppiaineRakenne.oppiaineet, 'id');
-    var utils = epLukioUtils;
-    var tabService = epLukioTabService;
-    $scope.oppiaineetJaOppimaarat = utils.flattenAndZipOppiaineet($scope.oppiaineRakenne.oppiaineet);
+    $scope.oppiaineetJaOppimaarat = epLukioUtils.flattenAndZipOppiaineet($scope.oppiaineRakenne.oppiaineet);
     TermistoService.setPeruste(peruste);
 
     function clickHandler(event) {
@@ -112,9 +110,9 @@ angular.module('eperusteet.esitys')
     };
 
 
-    $scope.tabs = tabService.tabs;
+    $scope.tabs = epLukioTabService.tabs;
     $scope.kurssiTyypit = ['pakollinen', 'syventava', 'soveltava'];
-    $scope.tabClass = tabService.tabClassSelector;
+    $scope.tabClass = epLukioTabService.tabClassSelector;
 
     MurupolkuData.set({perusteId: peruste.id, perusteNimi: peruste.nimi});
 
