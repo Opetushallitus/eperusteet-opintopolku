7b72ea59d920103fcbc741c3143bbce7109a68ad
Sauli Anto <sauli.anto@gmail.com>
2019-10-14 15:24:55 +0300

    WIP

--- eperusteet-opintopolku-app/v2/src/stores/PerusteDataStore.ts
+++ eperusteet-opintopolku-app/v2/src/stores/PerusteDataStore.ts
@@ -1,12 +1,14 @@
 import { Store, State } from '@shared/stores/store';
-import { PerusteDto } from '@shared/api/tyypit';
-import { Perusteet } from '@shared/api/eperusteet';
+import { PerusteenOsaViiteDtoObject, PerusteDto } from '@shared/api/tyypit';
+import { Perusteet, Sisallot } from '@shared/api/eperusteet';
 
 
 @Store
 export class PerusteDataStore {
   @State() public peruste: PerusteDto | null = null;
   @State() public perusteId: number | null = null;
+  @State() public sisalto: PerusteenOsaViiteDtoObject | null = null;
+  @State() public suoritustapa: string | null = null;
 
   constructor(perusteId?: number) {
     this.perusteId = perusteId || null;
@@ -17,6 +19,16 @@ export class PerusteDataStore {
       this.peruste = (await Perusteet.getPerusteenTiedot(this.perusteId)).data;
     } else {
       throw new Error('peruste-id-puuttuu');
+    }
+  }
+
+  async initSisalto() {
+    if (this.perusteId && this.peruste) {
+      const suoritustavat = this.peruste.suoritustavat;
+      console.log(suoritustavat);
+      this.sisalto = (await Sisallot.getSuoritustapaSisaltoUUSI(this.perusteId, 'LUKIOKOULUTUS2019')).data;
+    } else {
+      throw new Error('peruste-id-tai-peruste-puuttuu');
     }
   }
 }
