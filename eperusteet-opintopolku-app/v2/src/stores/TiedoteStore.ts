24a01ec8caf791221de4d74dcaac8c1597cf3498
Sauli Anto <sauli.anto@gmail.com>
2019-10-17 23:56:58 +0300

    Paranneltu tiedotteet näkymää

--- eperusteet-opintopolku-app/v2/src/stores/TiedoteStore.ts
+++ eperusteet-opintopolku-app/v2/src/stores/TiedoteStore.ts
@@ -1,5 +1,6 @@
+import _ from 'lodash';
 import { Store, Getter, State } from '@shared/stores/store';
-import { PageTiedoteDto, TiedoteDto } from '@shared/api/tyypit';
+import { TiedoteDto } from '@shared/api/tyypit';
 import { Tiedotteet } from '@shared/api/eperusteet';
 
 
@@ -7,38 +8,42 @@ import { Tiedotteet } from '@shared/api/eperusteet';
 export class TiedoteStore {
   @State() public uusimmatTiedotteet: TiedoteDto[] | null = null;
   @State() public tiedotteet: TiedoteDto[] | null = null;
-  @State() public lang = 'fi';
-  @State() public page = 0;
-  @State() public perPage = 0;
+  @State() public filter = {
+    nimi: '',
+    kieli: [ 'fi' ],
+    sivu: 0,
+    sivukoko: 10,
+  };
   @State() public amount = 0;
 
   @Getter()
   public data() {
     return {
       tiedotteet: this.tiedotteet,
-      lang: this.lang,
-      page: this.page,
-      perPage: this.perPage,
+      filter: this.filter,
       amount: this.amount,
     };
   }
 
   async getUusimmat() {
-    this.uusimmatTiedotteet = ((await Tiedotteet.findTiedotteetBy(0, 5, [this.lang], undefined, undefined, undefined, true, true)).data as any).data;
+    this.uusimmatTiedotteet = ((await Tiedotteet.findTiedotteetBy(
+      0, 5, this.filter.kieli, undefined, undefined, undefined, true, true)).data as any).data;
   }
 
-  async getUutiset(sivu, sivukoko, lang) {
-    if (lang === this.lang && this.page === sivu && this.tiedotteet) {
-      return this.tiedotteet;
-    }
-    else {
-      const result = (await Tiedotteet.findTiedotteetBy(
-        sivu, sivukoko, [lang], undefined, undefined, undefined, true, true)).data as any;
-      this.lang = lang;
-      this.page = result.sivu;
-      this.perPage = result.sivukoko;
-      this.amount = result['kokonaismäärä'];
-      this.tiedotteet = result.data;
-    }
+  public readonly updateFilter = _.debounce(async (filter) => {
+    this.filter = {
+      ...this.filter,
+      ...filter,
+    };
+    this.tiedotteet = null;
+    this.getUutiset();
+  }, 300);
+
+  async getUutiset() {
+    const result = (await Tiedotteet.findTiedotteetBy(
+      this.filter.sivu, this.filter.sivukoko, this.filter.kieli, this.filter.nimi,
+      undefined, undefined, true, true)).data as any;
+    this.amount = result['kokonaismäärä'];
+    this.tiedotteet = result.data;
   }
 }
