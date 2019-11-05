dc87ae91d9dcff9e69294c44845be08ac65baad7
nkala <niko.junkala@gofore.com>
2019-11-05 13:46:02 +0200

    v2 -> beta

--- eperusteet-opintopolku-app/v2/vue.config.js
+++ eperusteet-opintopolku-app/v2/vue.config.js
@@ -57,7 +57,7 @@ const proxy = {
 
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/v2/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/beta/' : '/',
   configureWebpack: {
     resolve: {
       alias: {
