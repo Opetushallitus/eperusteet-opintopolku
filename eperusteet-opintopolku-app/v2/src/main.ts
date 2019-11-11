8385e63b9bacf2dd1de10bcb24c667dc6e8008bf
nkala <niko.junkala@gofore.com>
2019-11-11 14:44:43 +0200

    Käytetään lokalisointipalvelua

--- eperusteet-opintopolku-app/v2/src/main.ts
+++ eperusteet-opintopolku-app/v2/src/main.ts
@@ -1,16 +1,25 @@
 import Vue from 'vue';
 
+import { Kielet, KieliStore } from '@shared/stores/kieli';
 import { rootConfig  } from '@/mainvue';
 import { createLogger } from '@shared/utils/logger';
+import { Lokalisointi } from '@shared/api/eperusteet';
 import '@/config/styles';
 const logger = createLogger('Main');
 
 Vue.config.productionTip = false;
 
 
+async function getKaannokset() {
+  const [fi, sv, en] = await Promise.all(
+    _.map(['fi', 'sv', 'en'], lang => Lokalisointi.getAllKaannokset(lang)));
+  return { fi, sv, en };
+}
+
 async function main() {
   try {
     logger.info('Mounting #app');
+    KieliStore.load(getKaannokset);
     (new Vue(rootConfig)).$mount('#app');
   }
   catch (err) {
