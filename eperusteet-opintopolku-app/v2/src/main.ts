c6529022ac54c7a9b4eeeec171540c89d87f3dc7
nkala <niko.junkala@gofore.com>
2019-11-11 17:03:17 +0200

    Lisätty puuttuva riippuvuus

--- eperusteet-opintopolku-app/v2/src/main.ts
+++ eperusteet-opintopolku-app/v2/src/main.ts
@@ -4,6 +4,7 @@ import { Kielet, KieliStore } from '@shared/stores/kieli';
 import { rootConfig  } from '@/mainvue';
 import { createLogger } from '@shared/utils/logger';
 import { Lokalisointi } from '@shared/api/eperusteet';
+import * as _ from 'lodash';
 import '@/config/styles';
 const logger = createLogger('Main');
 
@@ -13,7 +14,7 @@ Vue.config.productionTip = false;
 async function getKaannokset() {
   const [fi, sv, en] = await Promise.all(
     _.map(['fi', 'sv', 'en'], lang => Lokalisointi.getAllKaannokset(lang)));
-  return { fi, sv, en };
+  return { fi, sv, en } as any;
 }
 
 async function main() {
