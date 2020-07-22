34bba308b1243719d019f02c502c48719adc5f64
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-07-22 17:15:47 +0300

    EP-2301

--- eperusteet-opintopolku-app/v2/src/components/EpAmmatillinen/EpTutkinnonosaTutke.vue
+++ eperusteet-opintopolku-app/v2/src/components/EpAmmatillinen/EpTutkinnonosaTutke.vue
@@ -5,33 +5,7 @@
     </ep-form-content>
 
     <ep-form-content class="col-md-12 mb-5" name="osa-alueet" v-if="tutkinnonosa.osaAlueet">
-
-      <ep-collapse class="mb-3" v-for="(osaalue, index) in tutkinnonosa.osaAlueet" :key="'osaalue'+index"
-        :shadow="true"
-        :togglefull="true"
-        :borderBottom="false"
-        :expandedByDefault="tutkinnonosa.osaAlueet.length === 1">
-
-        <label class="osaamistavoiteotsikko" slot="header">{{$kaanna(osaalue.nimi)}}</label>
-
-        <div class="mt-2" v-for="(osaamistavoite, otIndex) in osaalue.osaamistavoitteet" :key="'osaamistavoite'+ index + otIndex">
-          <div class="osaamistavoiteotsikko">
-            <span v-if="osaamistavoite.pakollinen">{{$t('pakolliset-osaamistavoitteet')}}</span>
-            <span v-else>{{$t('valinnaiset-osaamistavoitteet')}}</span>
-            <span>, {{osaamistavoite.laajuus}} {{$t('osaamispiste')}}</span>
-          </div>
-
-          <div class="mt-2" v-html="$kaanna(osaamistavoite.tavoitteet)"></div>
-
-          <ep-ammatillinen-arvioinnin-kohdealueet
-            v-if="osaamistavoite.arviointi && osaamistavoite.arviointi.arvioinninKohdealueet"
-            :arviointiasteikot="arviointiasteikot"
-            :arvioinninKohdealueet="osaamistavoite.arviointi.arvioinninKohdealueet"/>
-
-        </div>
-
-      </ep-collapse>
-
+      <ep-ammatillinen-osaalueet :arviointiasteikot="arviointiasteikot" :osaalueet="tutkinnonosa.osaAlueet" />
     </ep-form-content>
 
   </div>
@@ -42,12 +16,14 @@ import { Component, Vue, Prop } from 'vue-property-decorator';
 import EpFormContent from '@shared/components/forms/EpFormContent.vue';
 import EpCollapse from '@shared/components/EpCollapse/EpCollapse.vue';
 import EpAmmatillinenArvioinninKohdealueet from '@/components/EpAmmatillinen/EpAmmatillinenArvioinninKohdealueet.vue';
+import EpAmmatillinenOsaalueet from '@/components/EpAmmatillinen/EpAmmatillinenOsaalueet.vue';
 
 @Component({
   components: {
     EpFormContent,
     EpCollapse,
     EpAmmatillinenArvioinninKohdealueet,
+    EpAmmatillinenOsaalueet,
   },
 })
 export default class EpTutkinnonosaTutke extends Vue {
@@ -56,24 +32,9 @@ export default class EpTutkinnonosaTutke extends Vue {
 
   @Prop({ required: true })
   private arviointiasteikot!: any[];
-
-  get osaamistasonKriteeritFields() {
-    return [{
-      key: 'osaamistaso',
-      label: this.$t('osaamistaso') as string,
-      thStyle: { width: '40%' },
-    }, {
-      key: 'kriteerit',
-      label: this.$t('kriteerit') as string,
-    }] as any[];
-  }
 }
 </script>
 
 <style scoped lang="scss">
-
-  .osaamistavoiteotsikko {
-    font-weight: 600;
-  }
 
 </style>
