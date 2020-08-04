d4c7b0c5ede9ffe482122ee1719df453c141e87b
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-08-04 16:28:57 +0300

    frontend update

--- eperusteet-opintopolku-app/v2/src/utils/peruste.ts
+++ eperusteet-opintopolku-app/v2/src/utils/peruste.ts
@@ -1,6 +1,7 @@
 import { PerusteDtoToteutusEnum as PerusteToteutusEnum } from '@shared/generated/eperusteet';
 import { PerusteDtoToteutusEnum as YlopsToteutusEnum } from '@shared/generated/ylops';
 import { KoulutustyyppiToteutus, Koulutustyyppi } from '@shared/tyypit';
+import { koulutustyyppiTheme } from '@shared/utils/perusteet';
 
 interface HasKoulutustyyppiToteutus {
   koulutustyyppi?: string | null;
@@ -15,5 +16,6 @@ export function uusiJulkinenToteutus(hasToteutus: HasKoulutustyyppiToteutus) {
         || hasToteutus.toteutus === KoulutustyyppiToteutus.lops2019.valueOf()
         || hasToteutus.toteutus === KoulutustyyppiToteutus.tpo.valueOf()
         || hasToteutus.toteutus === KoulutustyyppiToteutus.perusopetus.valueOf()
-        || hasToteutus.koulutustyyppi === Koulutustyyppi.aikuistenperusopetus);
+        || hasToteutus.koulutustyyppi === Koulutustyyppi.aikuistenperusopetus
+        || koulutustyyppiTheme(hasToteutus.koulutustyyppi as string) === 'ammatillinen');
 }
