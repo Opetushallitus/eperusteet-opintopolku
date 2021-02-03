88b7d4d82178f8d304b1fbc1c32b2c58f897a9dc
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-02-03 12:57:08 +0200

    EP-2567

--- eperusteet-opintopolku-app/v2/src/App.vue
+++ eperusteet-opintopolku-app/v2/src/App.vue
@@ -9,9 +9,29 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import * as _ from 'lodash';
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  mounted() {
+    if (!this.localDev) {
+      let recaptchaScript = document.createElement('script');
+      recaptchaScript.setAttribute('id', 'apply-modal');
+      recaptchaScript.setAttribute('type', 'text/javascript');
+      recaptchaScript.setAttribute('src', this.opintopolkuUrl + '/oppija-raamit/js/apply-modal.js');
+      document.head.appendChild(recaptchaScript);
+    }
+  }
+
+  get localDev() {
+    return _.includes(window.location.origin, 'localhost');
+  }
+
+  get opintopolkuUrl() {
+    const origin = window.location.origin;
+    return origin.replace('eperusteet.', '');
+  }
+}
 </script>
 
 <style lang="scss" src="@shared/styles/app.scss"></style>
