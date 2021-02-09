75b17c56e5d6a3ee4f3a068639c5d58b85e4fcc3
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-02-09 12:27:32 +0200

    EP-2635

--- eperusteet-opintopolku-app/v2/src/stores/PerusopetusOppiaineStore.ts
+++ eperusteet-opintopolku-app/v2/src/stores/PerusopetusOppiaineStore.ts
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import VueCompositionApi, { reactive, computed, ref, watch } from '@vue/composition-api';
-import { PerusopetuksenPerusteenSisalto, OppiaineDto } from '@shared/api/eperusteet';
-import _ from 'lodash';
+import { PerusopetuksenPerusteenSisalto, OppiaineDto, LaajaalainenOsaaminenDto } from '@shared/api/eperusteet';
+import * as _ from 'lodash';
+import { Kielet } from '@shared/stores/kieli';
 
 Vue.use(VueCompositionApi);
 
@@ -23,16 +24,35 @@ export class PerusopetusOppiaineStore {
 
   public async init() {
     const vuosiluokkakokonaisuudetById = _.keyBy((await PerusopetuksenPerusteenSisalto.getVuosiluokkaKokonaisuudet(this.perusteId)).data, 'id');
+    const laajaalaisetOsaamiset = _.keyBy((await (PerusopetuksenPerusteenSisalto.getOsaamiset(this.perusteId))).data, 'id');
 
     this.state.oppiaine = (await PerusopetuksenPerusteenSisalto.getPerusopetusOppiaine(this.perusteId, this.oppiaineId)).data;
     this.state.oppiaine = {
       ...this.state.oppiaine,
       vuosiluokkakokonaisuudet: _.chain(this.state.oppiaine.vuosiluokkakokonaisuudet)
         .map(vlk => {
+          const vlkSisaltoalueetById = _.keyBy(vlk.sisaltoalueet, 'id');
           return {
             ...vlk,
             nimi: _.get(vuosiluokkakokonaisuudetById[_.get(vlk, '_vuosiluokkaKokonaisuus')], 'nimi'),
             vuosiluokat: _.get(vuosiluokkakokonaisuudetById[_.get(vlk, '_vuosiluokkaKokonaisuus')], 'vuosiluokat'),
+            tavoitteet: _.map(vlk.tavoitteet, tavoite => {
+              return {
+                ...tavoite,
+                laajattavoitteet: _.chain(tavoite.laajattavoitteet)
+                  .map((laajatavoitet: string) => {
+                    return laajaalaisetOsaamiset[laajatavoitet] as LaajaalainenOsaaminenDto;
+                  })
+                  .sortBy((ltavoite: any) => ltavoite.nimi[Kielet.getSisaltoKieli.value])
+                  .value() as any,
+                sisaltoalueet: _.chain(tavoite.sisaltoalueet)
+                  .map((sisaltoalue: string) => {
+                    return vlkSisaltoalueetById[sisaltoalue];
+                  })
+                  .sortBy((sisaltoalue: any) => sisaltoalue.nimi[Kielet.getSisaltoKieli.value])
+                  .value() as any,
+              };
+            }),
           };
         })
         .sortBy('vuosiluokat')
