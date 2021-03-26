7f6976dd9cdb77d53774ee5fa721a1f891be416f
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-03-26 10:53:44 +0200

    EP-2674

--- eperusteet-opintopolku-app/v2/src/routes/uutiset/RouteUutinen.vue
+++ eperusteet-opintopolku-app/v2/src/routes/uutiset/RouteUutinen.vue
@@ -16,16 +16,43 @@
     <ep-spinner v-else />
     </template>
     <div v-if="tiedote" class="tiedote">
-      <div class="mb-4" v-if="perusteRoutes.length > 0">
-        {{$t('katso-peruste')}}:
-        <span class="peruste" v-for="(peruste, index) in perusteRoutes" :key="'peruste'+peruste.id">
-          <span v-if="index > 0">, </span>
-          <router-link :to="peruste.route">{{$kaanna(peruste.nimi)}}</router-link>
-        </span>
+
+      <div class="sisalto pb-4">
+        <ep-content-viewer :value="$kaanna(tiedote.sisalto)"/>
       </div>
 
-      <div class="sisalto">
-        <ep-content-viewer :value="$kaanna(tiedote.sisalto)"/>
+      <div
+        class="tiedote-lisainfo p-3"
+        v-if="tiedoteMapped.perusteet.length > 0 || tiedoteMapped.tutkinnonosat.length > 0 || tiedoteMapped.osaamisalat.length > 0">
+
+        <h3>{{$t('tiedotteeseen-liittyy')}}</h3>
+
+        <div class="mt-4" v-if="tiedoteMapped.perusteet.length > 0">
+          <h4>{{$t('perusteet')}}</h4>
+          <span class="peruste" v-for="(peruste, index) in tiedoteMapped.perusteet" :key="'peruste'+peruste.id">
+            <span v-if="index > 0">, </span>
+            <router-link :to="peruste.route">{{$kaanna(peruste.nimi)}}</router-link>
+          </span>
+        </div>
+
+        <ep-spinner v-if="!tutkinnonosienPerusteet" />
+        <uutisen-koodit
+          v-else
+          :kooditPerusteilla="tutkinnonosienPerusteet"
+          class="mt-4">
+          <h4 slot="header">{{$t('tutkinnonosat')}}</h4>
+          <h4 slot="popover-header">{{$t('perusteet-joissa-tutkinnon-osia-on')}}</h4>
+        </uutisen-koodit>
+
+        <ep-spinner v-if="!osaamisalojenPerusteet" />
+        <uutisen-koodit
+          v-else
+          :kooditPerusteilla="osaamisalojenPerusteet"
+          class="mt-4">
+          <h4 slot="header">{{$t('osaamisalat')}}</h4>
+          <h4 slot="popover-header">{{$t('perusteet-joissa-osaamisaloja-on')}}</h4>
+        </uutisen-koodit>
+
       </div>
     </div>
     <ep-spinner v-else />
@@ -43,6 +70,8 @@ import EpHeader from '@/components/EpHeader/EpHeader.vue';
 import EpContentViewer from '@shared/components/EpContentViewer/EpContentViewer.vue';
 import { Meta } from '@shared/utils/decorators';
 import { koulutustyyppiStateName } from '@shared/utils/perusteet';
+import EpButton from '@shared/components/EpButton/EpButton.vue';
+import UutisenKoodit from './UutisenKoodit.vue';
 
 @Component({
   components: {
@@ -50,6 +79,8 @@ import { koulutustyyppiStateName } from '@shared/utils/perusteet';
     EpHeader,
     EpSearch,
     EpContentViewer,
+    EpButton,
+    UutisenKoodit,
   },
 })
 export default class RouteUutinen extends Vue {
@@ -68,12 +99,27 @@ export default class RouteUutinen extends Vue {
     return this.tiedoteStore.tiedote;
   }
 
+  get tiedoteMapped() {
+    return {
+      ...this.tiedote,
+      perusteet: this.perusteRoutes(this.perusteet),
+    };
+  }
+
   get perusteet() {
     return this.tiedoteStore.tiedote?.perusteet;
   }
 
-  get perusteRoutes() {
-    return _.map(this.perusteet, peruste => {
+  get osaamisalojenPerusteet() {
+    return this.tiedoteStore.tiedotteenOsaamisalaPerusteet;
+  }
+
+  get tutkinnonosienPerusteet() {
+    return this.tiedoteStore.tiedotteenTutkinnonosaPerusteet;
+  }
+
+  perusteRoutes(perusteet) {
+    return _.map(perusteet, peruste => {
       return {
         ...peruste,
         route: {
@@ -143,5 +189,11 @@ export default class RouteUutinen extends Vue {
   .sisalto {
     @include teksti-sisalto;
   }
+
+  .tiedote-lisainfo {
+    border: 1px solid $gray-lighten-3;
+    border-radius: 3px;
+  }
+
 }
 </style>
--- eperusteet-opintopolku-app/v2/src/stores/TiedoteStore.ts
+++ eperusteet-opintopolku-app/v2/src/stores/TiedoteStore.ts
@@ -1,6 +1,12 @@
 import _ from 'lodash';
 import { Store, Getter, State } from '@shared/stores/store';
-import { TiedoteDto, Tiedotteet } from '@shared/api/eperusteet';
+import { TiedoteDto, Tiedotteet, Perusteet, getAllPerusteet, PerusteInfoDto, KoodiDto } from '@shared/api/eperusteet';
+import { Page } from '@shared/tyypit';
+import { ActionPayload } from 'vuex';
+
+export interface KoodiPerusteella extends KoodiDto {
+  perusteet: PerusteInfoDto[];
+};
 
 @Store
 export class TiedoteStore {
@@ -15,6 +21,8 @@ export class TiedoteStore {
     sivu: 0,
     sivukoko: 10,
   };
+  @State() public tiedotteenTutkinnonosaPerusteet: KoodiPerusteella[] | null = null;
+  @State() public tiedotteenOsaamisalaPerusteet: KoodiPerusteella[] | null = null;
 
   async getUusimmat() {
     this.uusimmatTiedotteet = ((await Tiedotteet.findTiedotteetBy(
@@ -41,6 +49,33 @@ export class TiedoteStore {
   async fetchUutinen(tiedoteId: number) {
     this.tiedoteId = tiedoteId;
     this.tiedote = null;
+    this.tiedotteenTutkinnonosaPerusteet = null;
+    this.tiedotteenOsaamisalaPerusteet = null;
+
     this.tiedote = (await Tiedotteet.getTiedote(this.tiedoteId)).data;
+
+    if (!_.isEmpty(this.tiedote?.tutkinnonosat)) {
+      this.tiedotteenTutkinnonosaPerusteet = await Promise.all(_.map(this.tiedote?.tutkinnonosat, async tutkinnonosa => {
+        return {
+          ...tutkinnonosa,
+          perusteet: ((await getAllPerusteet({ tutkinnonosaKoodit: [tutkinnonosa.uri!] })).data as Page<PerusteInfoDto>).data,
+        };
+      }));
+    }
+    else {
+      this.tiedotteenTutkinnonosaPerusteet = [];
+    }
+
+    if (!_.isEmpty(this.tiedote?.osaamisalat)) {
+      this.tiedotteenOsaamisalaPerusteet = await Promise.all(_.map(this.tiedote?.osaamisalat, async osaamisala => {
+        return {
+          ...osaamisala,
+          perusteet: ((await getAllPerusteet({ osaamisalaKoodit: [osaamisala.uri!] })).data as Page<PerusteInfoDto>).data,
+        };
+      }));
+    }
+    else {
+      this.tiedotteenOsaamisalaPerusteet = [];
+    }
   }
 }
