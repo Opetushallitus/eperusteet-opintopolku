b0b5b86a16350c20ed4e38e642addde0e4920745
oplekal <53811784+oplekal@users.noreply.github.com>
2021-04-20 12:37:46 +0300

    EP-2704 (#386)
    * EP-2704
    
    * tests fix

--- eperusteet-opintopolku-app/v2/src/routes/Root.spec.ts
+++ eperusteet-opintopolku-app/v2/src/routes/Root.spec.ts
@@ -1,19 +1,24 @@
 import { mount, createLocalVue } from '@vue/test-utils';
 import Root from './Root.vue';
 import { Kielet } from '@shared/stores/kieli';
-import { mocks, stubs } from '@shared/utils/jestutils';
+import { mock, mocks, stubs } from '@shared/utils/jestutils';
 import VueI18n from 'vue-i18n';
 import { Kaannos } from '@shared/plugins/kaannos';
+import { PalauteStore } from '@/stores/PalauteStore';
 
 describe('Root', () => {
   const localVue = createLocalVue();
   localVue.use(VueI18n);
   Kielet.install(localVue);
   localVue.use(new Kaannos());
+  const palauteStore = mock(PalauteStore);
 
   test('Renders', async () => {
     const wrapper = mount(Root as any, {
       localVue,
+      propsData: {
+        palauteStore,
+      },
       stubs: {
         ...stubs,
         RouterView: true,
