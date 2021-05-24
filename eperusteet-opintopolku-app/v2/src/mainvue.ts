e56318894fc5784d5f8602efdefcfed419f1863e
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-05-24 09:26:01 +0300

    EP-2754

--- eperusteet-opintopolku-app/v2/src/mainvue.ts
+++ eperusteet-opintopolku-app/v2/src/mainvue.ts
@@ -82,12 +82,12 @@ Vue.use(SocialSharing);
 Vue.use(Kielet, {
   messages: {
     fi: {
-      ...require('@shared/translations/locale-fi.json'),
       ...require('@/translations/locale-fi.json'),
+      ...require('@shared/translations/locale-fi.json'),
     },
     sv: {
-      ...require('@shared/translations/locale-sv.json'),
       ...require('@/translations/locale-sv.json'),
+      ...require('@shared/translations/locale-sv.json'),
     },
   },
 });
