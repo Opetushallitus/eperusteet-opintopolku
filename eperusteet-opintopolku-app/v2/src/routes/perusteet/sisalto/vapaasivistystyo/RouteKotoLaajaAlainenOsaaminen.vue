05b92e5b5bf5e7547e90f843d4862ebd6387cbc5
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2022-03-29 18:48:07 +0300

    EP-3083: koton laaja-alainen lisätty routeriin + runkoa routelle

---
+++ eperusteet-opintopolku-app/v2/src/routes/perusteet/sisalto/vapaasivistystyo/RouteKotoLaajaAlainenOsaaminen.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>TODO tahan tulee laaja-alainen osaaminen</div>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue';
+
+export default class RouteKotoLaajaAlainenOsaaminen extends Vue {
+
+}
+</script>
