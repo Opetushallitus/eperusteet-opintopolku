ff148d9c11c132b56069b1f8413b2a760d6ccf06
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2022-03-29 19:34:31 +0300

    EP-3083: Perusteenosan nimen rendaus koto laaja-alaiselle

--- eperusteet-opintopolku-app/v2/src/routes/perusteet/sisalto/vapaasivistystyo/RouteKotoLaajaAlainenOsaaminen.vue
+++ eperusteet-opintopolku-app/v2/src/routes/perusteet/sisalto/vapaasivistystyo/RouteKotoLaajaAlainenOsaaminen.vue
@@ -1,12 +1,57 @@
 <template>
-  <div>TODO tahan tulee laaja-alainen osaaminen</div>
+  <div class="content">
+    <div v-if="perusteenOsa">
+      <h2 id="tekstikappale-otsikko" class="otsikko mb-4">{{ $kaanna(perusteenOsa.nimi) }}</h2>
+    </div>
+    <ep-spinner v-else />
+  </div>
 </template>
 
 <script lang="ts">
 
 import Vue from 'vue';
+import { Component, Prop, Watch } from 'vue-property-decorator';
+import { PerusteDataStore } from '@/stores/PerusteDataStore';
+import { PerusteenOsaStore } from '@/stores/PerusteenOsaStore';
+import EpSpinner from '@shared/components/EpSpinner/EpSpinner.vue';
 
+@Component({
+  components: {
+    EpSpinner,
+  },
+})
 export default class RouteKotoLaajaAlainenOsaaminen extends Vue {
+  @Prop({ required: true })
+  private perusteDataStore!: PerusteDataStore;
 
+  @Prop({ required: true })
+  private perusteenOsaStore!: PerusteenOsaStore;
+
+  @Watch('current', { immediate: true })
+  async fetchAlikappaleet() {
+    if (!this.current) {
+      return;
+    }
+
+    const isMainHeading = !!this.current && this.current.path.length === 2;
+    await this.perusteenOsaStore.fetchPerusteenOsa(isMainHeading);
+  }
+
+  get perusteenOsa() {
+    return this.perusteenOsaStore.perusteenOsa;
+  }
+
+  get current() {
+    return this.perusteDataStore.current || null;
+  }
 }
 </script>
+
+<style scoped lang="scss">
+@import '@shared/styles/_variables.scss';
+@import '@shared/styles/_mixins.scss';
+
+.content {
+  padding: 0 $content-padding;
+}
+</style>
