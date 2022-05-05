b4954b30d3021216f31f1e76b1058a6035e2219b
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2022-05-05 09:33:33 +0300

    beta-redirect-fix

---
+++ eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/RedirectServlet.java
@@ -0,0 +1,19 @@
+package fi.vm.sade.eperusteet.opintopolku;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/beta/*"}, loadOnStartup = 1)
+public class RedirectServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestUri = request.getRequestURI();
+        response.sendRedirect("/" + requestUri.substring(requestUri.indexOf("beta/")+ 5 ));
+    }
+
+}
