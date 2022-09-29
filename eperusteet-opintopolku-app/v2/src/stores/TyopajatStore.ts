25c07c443d5a346ea3066e055cd6c15a6635dd7b
timopaasila <113356513+timopaasila@users.noreply.github.com>
2022-09-29 15:05:05 +0300

    EP-3320 - korjattu kielivalinnan vaikutus työpajojen hakutuloksiin (#540)
    * EP-3320 - korjattu kielivalinnan vaikutus työpajojen hakutuloksiin
    
    * EP-3320 - korjattu kielivalinnan vaikutus työpajojen hakutuloksiin

--- eperusteet-opintopolku-app/v2/src/stores/TyopajatStore.ts
+++ eperusteet-opintopolku-app/v2/src/stores/TyopajatStore.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
-import VueCompositionApi, { reactive, computed, ref, watch } from '@vue/composition-api';
-import _ from 'lodash';
-import { JulkinenApi, OpetussuunnitelmaDto } from '@shared/api/amosaa';
+import VueCompositionApi, { reactive, computed } from '@vue/composition-api';
+import { getJulkisetOpetussuunnitelmat, OpetussuunnitelmaDto } from '@shared/api/amosaa';
 import { Kielet } from '@shared/stores/kieli';
 
 Vue.use(VueCompositionApi);
@@ -14,16 +13,11 @@ export class TyopajatStore {
   public readonly opetussuunnitelmat = computed(() => this.state.opetussuunnitelmat);
 
   public async fetch() {
-    this.state.opetussuunnitelmat = ((await JulkinenApi.findOpetussuunnitelmat(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      0,
-      100,
-      '',
-      Kielet.getUiKieli.value,
-      true,
-    )).data as any).data;
+    this.state.opetussuunnitelmat = ((await getJulkisetOpetussuunnitelmat({
+      sivu: 0,
+      sivukoko: 100,
+      kieli: Kielet.getUiKieli.value,
+      organisaatioRyhma: true,
+    })).data as any).data;
   }
 }
