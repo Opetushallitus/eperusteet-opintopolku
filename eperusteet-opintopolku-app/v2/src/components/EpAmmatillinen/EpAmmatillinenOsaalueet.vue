d4b9a89e8b18acd55d7ea81b355b59f3de6409cc
Timo Paasila <timo.paasila@gofore.com>
2022-10-20 09:19:22 +0300

    EP-3371

--- eperusteet-opintopolku-app/v2/src/components/EpAmmatillinen/EpAmmatillinenOsaalueet.vue
+++ eperusteet-opintopolku-app/v2/src/components/EpAmmatillinen/EpAmmatillinenOsaalueet.vue
@@ -5,7 +5,7 @@
       :borderBottom="false"
       :expandedByDefault="osaalueet.length === 1">
 
-      <h3 class="osaamistavoiteotsikko" slot="header">{{$kaanna(osaalue.nimi)}} <span v-if="osaalue.koodi">({{$t(osaalue.koodi.arvo)}})</span></h3>
+      <h3 class="osaamistavoiteotsikko" slot="header">{{$kaanna(osaalue.nimi)}} <span v-if="osaalue.koodi">({{osaalue.koodi.arvo}})</span></h3>
 
       <div class="mt-2" v-for="(osaamistavoite, otIndex) in osaalue.osaamistavoitteet" :key="'osaamistavoite'+ index + otIndex">
         <div class="osaamistavoiteotsikko">
