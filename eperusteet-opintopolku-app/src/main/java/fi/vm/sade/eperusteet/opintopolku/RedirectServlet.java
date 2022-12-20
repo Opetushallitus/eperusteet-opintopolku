a0fcaa928c0b351b92ab378a3ba692b77bedfb4e
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2022-12-20 13:25:35 +0200

    EP-3463

--- eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/RedirectServlet.java
+++ eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/RedirectServlet.java
@@ -1,19 +1,27 @@
 package fi.vm.sade.eperusteet.opintopolku;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/beta/*"}, loadOnStartup = 1)
+@WebServlet(urlPatterns = {"/beta/*", "/vanha-ui/*"}, loadOnStartup = 1)
 public class RedirectServlet extends HttpServlet {
+
+    private static final List<String> redirects = Arrays.asList("/beta", "/vanha-ui");
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String requestUri = request.getRequestURI();
-        response.sendRedirect("/" + requestUri.substring(requestUri.indexOf("beta/")+ 5 ));
-    }
 
+        for (String redirect : redirects) {
+            if (requestUri.contains(redirect)) {
+                response.sendRedirect(requestUri.substring(requestUri.indexOf(redirect) + redirect.length()));
+            }
+        }
+    }
 }
