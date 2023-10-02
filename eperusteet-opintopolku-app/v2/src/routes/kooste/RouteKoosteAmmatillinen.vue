01c77779ee0032d14efd989433a3f4c98a30e5a9
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-10-02 15:43:39 +0300

    EP-3912

--- eperusteet-opintopolku-app/v2/src/routes/kooste/RouteKoosteAmmatillinen.vue
+++ eperusteet-opintopolku-app/v2/src/routes/kooste/RouteKoosteAmmatillinen.vue
@@ -1,72 +1,74 @@
 <template>
-  <ep-spinner v-if="!peruste" />
-  <ep-header :murupolku="murupolku" :koulutustyyppi="koulutustyyppi" v-else>
-    <template slot="header">
-      {{ $kaanna(peruste.nimi) }} <span v-if="peruste.laajuus">{{peruste.laajuus}} {{$t('osaamispiste')}}</span>
-    </template>
-    <div class="selaus">
-      <b-container fluid>
-        <b-row class="mb-0">
-          <b-col cols="12" lg="6" class="tile mb-5">
-            <h2 class="otsikko mb-4">{{ $t('peruste') }}</h2>
-            <router-link :to="perusteRoute">
-              <peruste-tile :julkaisu="peruste" :koulutustyyppi="koulutustyyppi"></peruste-tile>
-            </router-link>
-          </b-col>
-          <b-col cols="12" lg="6" class="mb-5">
-            <h2 class="mb-4">{{$t('ajankohtaista')}}</h2>
-            <ep-spinner v-if="!tiedotteet"/>
-            <ep-julki-lista :tiedot="tiedotteet" @avaaTieto="avaaTiedote" v-else>
-              <template v-slot:lisaaBtnText>
-                <div class="mt-2">
-                  {{$t('katso-lisaa-ajankohtaisia')}}
+  <div id="scroll-anchor">
+    <ep-spinner v-if="!peruste" />
+    <ep-header :murupolku="murupolku" :koulutustyyppi="koulutustyyppi" v-else>
+      <template slot="header">
+        {{ $kaanna(peruste.nimi) }} <span v-if="peruste.laajuus">{{peruste.laajuus}} {{$t('osaamispiste')}}</span>
+      </template>
+      <div class="selaus">
+        <b-container fluid>
+          <b-row class="mb-0">
+            <b-col cols="12" lg="6" class="tile mb-5">
+              <h2 class="otsikko mb-4">{{ $t('peruste') }}</h2>
+              <router-link :to="perusteRoute">
+                <peruste-tile :julkaisu="peruste" :koulutustyyppi="koulutustyyppi"></peruste-tile>
+              </router-link>
+            </b-col>
+            <b-col cols="12" lg="6" class="mb-5">
+              <h2 class="mb-4">{{$t('ajankohtaista')}}</h2>
+              <ep-spinner v-if="!tiedotteet"/>
+              <ep-julki-lista :tiedot="tiedotteet" @avaaTieto="avaaTiedote" v-else>
+                <template v-slot:lisaaBtnText>
+                  <div class="mt-2">
+                    {{$t('katso-lisaa-ajankohtaisia')}}
+                  </div>
+                </template>
+                <template v-slot:eiTietoja>
+                  <div class="mt-2">
+                    {{$t('ei-tiedotteita')}}
+                  </div>
+                </template>
+              </ep-julki-lista>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <h2 class="otsikko">{{ $t('paikalliset-toteutussuunnitelmat') }}</h2>
+              <div class="search">
+                <div class="placeholder mb-2">{{$t('etsi-toteutussuunnitelmaa-nimella-tutkinnon-osalla-tai-organisaatiolla')}}</div>
+                <ep-search v-model="query" :placeholder="$t('etsi')"/>
+              </div>
+              <ep-spinner v-if="!opetussuunnitelmatPage" />
+              <div v-else-if="opetussuunnitelmat.length === 0 && !query">
+                <div class="alert alert-info">
+                  {{ $t('perusteen-pohjalta-ei-toteutettu-toteutussuunnitelmia') }}
                 </div>
-              </template>
-              <template v-slot:eiTietoja>
-                <div class="mt-2">
-                  {{$t('ei-tiedotteita')}}
+              </div>
+              <div v-else-if="opetussuunnitelmat.length === 0 && query">
+                <div class="alert alert-info">
+                  {{ $t('ei-hakutuloksia') }}
                 </div>
-              </template>
-            </ep-julki-lista>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col>
-            <h2 class="otsikko">{{ $t('paikalliset-toteutussuunnitelmat') }}</h2>
-            <div class="search">
-              <div class="placeholder mb-2">{{$t('etsi-toteutussuunnitelmaa-nimella-tutkinnon-osalla-tai-organisaatiolla')}}</div>
-              <ep-search v-model="query" :placeholder="$t('etsi')"/>
-            </div>
-            <ep-spinner v-if="!opetussuunnitelmatPage" />
-            <div v-else-if="opetussuunnitelmat.length === 0 && !query">
-              <div class="alert alert-info">
-                {{ $t('perusteen-pohjalta-ei-toteutettu-toteutussuunnitelmia') }}
               </div>
-            </div>
-            <div v-else-if="opetussuunnitelmat.length === 0 && query">
-              <div class="alert alert-info">
-                {{ $t('ei-hakutuloksia') }}
-              </div>
-            </div>
-            <div v-else id="opetussuunnitelmat-lista">
-              <div v-for="(ops, idx) in opetussuunnitelmat" :key="idx">
+              <div v-else id="opetussuunnitelmat-lista">
+                <div v-for="(ops, idx) in opetussuunnitelmat" :key="idx">
 
-                <router-link :to="ops.route">
-                  <opetussuunnitelma-tile :ops="ops" :query="query" :show-ops-icon="false"/>
-                </router-link>
+                  <router-link :to="ops.route">
+                    <opetussuunnitelma-tile :ops="ops" :query="query" :show-ops-icon="false"/>
+                  </router-link>
 
+                </div>
+                <EpBPagination v-model="page"
+                              :items-per-page="perPage"
+                              :total="opetussuunnitelmatPage.kokonaismäärä"
+                              aria-controls="opetussuunnitelmat-lista">
+                </EpBPagination>
               </div>
-              <EpBPagination v-model="page"
-                             :items-per-page="perPage"
-                             :total="opetussuunnitelmatPage.kokonaismäärä"
-                             aria-controls="opetussuunnitelmat-lista">
-              </EpBPagination>
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
-  </ep-header>
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
+    </ep-header>
+  </div>
 </template>
 
 <script lang="ts">
