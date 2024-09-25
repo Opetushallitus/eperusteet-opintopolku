6cc91c03e05ee08bb59542fa027ec13b8e041c58
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-09-25 13:38:35 +0300

    testing

--- eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/config/MvcConfiguration.java
+++ eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/config/MvcConfiguration.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/digiosaaminen", "/digiosaaminen/index.html");
+//        registry.addRedirectViewController("/testi", "/testi/index.html");
+        registry.addViewController("/testi").setViewName("forward:/testi/index.html");
+//        registry.addRedirectViewController("/testi", "/testi/");
     }
 
 }
--- eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/controller/DigiOsaaminenController.java
+++ eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/controller/DigiOsaaminenController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,35 +22,26 @@ public class DigiOsaaminenController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @GetMapping("/digitesti")
+    @ResponseBody
+    public String getDigiOsaaminenTest() {
+        return "digiOsaaminen";
+    }
+
     @GetMapping("/digiosaaminen")
-    public void getDigiOsaaminen(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public String getDigiOsaaminen(HttpServletRequest request, HttpServletResponse response) {
         log.debug("Redirecting to digiosaaminen");
 
         String url = request.getRequestURL().toString().replace(request.getRequestURI(), "");
         String apiUrl = url + "/eperusteet-service/api/perusteet/julkaisut?tyyppi=digitaalinen_osaaminen";
 
-        log.debug("API URL: {}", apiUrl);
-        try {
-            // Make the API call to fetch data
-            JsonNode digiOsaamiset = restTemplate.getForObject(apiUrl, JsonNode.class);
+        JsonNode digiOsaamiset = restTemplate.getForObject(apiUrl, JsonNode.class);
 
-            log.debug("API response: {}", digiOsaamiset);
-
-            // Check if the response contains exactly one "digiOsaaminen"
-            if (digiOsaamiset == null || digiOsaamiset.get("data").size() != 1) {
-                log.debug("No digiOsaaminen found");
-                response.sendError(HttpStatus.NOT_FOUND.value());
-                return;
-            }
-
-            // Extract the ID and redirect
-            long digiOsaaminenId = digiOsaamiset.get("data").get(0).get("id").asLong();
-
-            log.debug("Redirecting to /#/fi/digiosaaminen/{}", digiOsaaminenId);
-
-            response.sendRedirect(url + "/#/fi/digiosaaminen/" + digiOsaaminenId);
-        } catch(HttpClientErrorException.NotFound ex) {
-            response.sendError(HttpStatus.NOT_FOUND.value());
+        if (digiOsaamiset == null && digiOsaamiset.get("data").size() != 1) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
+
+        long digiOsaaminenId = digiOsaamiset.get("data").get(0).get("id").asLong();
+        return "redirect:" + url + "/#/fi/digiosaaminen/" + digiOsaaminenId;
     }
 }
