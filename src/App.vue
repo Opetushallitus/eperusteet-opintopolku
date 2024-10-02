395972571dc07e6b75c811d1169f85b7c6342136
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-10-02 13:10:06 +0300

    EP-4376-2

--- src/App.vue
+++ src/App.vue
@@ -7,30 +7,10 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import * as _ from 'lodash';
-import { Kielet } from '@shared/stores/kieli';
 
 @Component
 export default class App extends Vue {
-  mounted() {
-    if (!this.localDev) {
-      let recaptchaScript = document.createElement('script');
-      recaptchaScript.setAttribute('id', 'apply-modal');
-      recaptchaScript.setAttribute('type', 'text/javascript');
-      recaptchaScript.setAttribute('sdg', 'false');
-      recaptchaScript.setAttribute('lang', Kielet.getUiKieli.value);
-      recaptchaScript.setAttribute('src', this.opintopolkuUrl + '/oppija-raamit/js/apply-modal.js');
-      document.head.appendChild(recaptchaScript);
-    }
-  }
 
-  get localDev() {
-    return _.includes(window.location.origin, 'localhost');
-  }
-
-  get opintopolkuUrl() {
-    const origin = window.location.origin;
-    return origin.replace('eperusteet.', '');
-  }
 }
 </script>
 
