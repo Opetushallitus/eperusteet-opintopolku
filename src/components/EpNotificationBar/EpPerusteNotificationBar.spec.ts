e263b9afd9176c52821e5050e723f44e57c8bd40
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-11-05 13:12:55 +0200

    EP-3932-2

---
+++ src/components/EpNotificationBar/EpPerusteNotificationBar.spec.ts
@@ -0,0 +1,323 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import EpPerusteNotificationBar from './EpPerusteNotificationBar.vue';
+import { Kielet } from '@shared/stores/kieli';
+import { mocks, stubs } from '@shared/utils/jestutils';
+import VueI18n from 'vue-i18n';
+import { Kaannos } from '@shared/plugins/kaannos';
+import { Kieli } from '@shared/tyypit';
+
+describe('EpPerusteNotificationBar', () => {
+  const localVue = createLocalVue();
+  localVue.use(VueI18n);
+  localVue.use(Kielet, {
+    messages: {
+      fi: {
+        ...require('@shared/translations/locale-fi.json'),
+      },
+      sv: {
+        ...require('@shared/translations/locale-sv.json'),
+      },
+    },
+  });
+  localVue.use(new Kaannos());
+
+  Kielet.setUiKieli(Kieli.fi);
+
+  const datePlusDays = (days: number) => {
+    const date = new Date();
+    date.setDate(date.getDate() + days);
+    return date;
+  };
+
+  const peruste = {
+    voimassaoloAlkaa: datePlusDays(0).getTime(),
+    kielet: ['fi', 'sv'],
+  };
+
+  const komponenttiStubit = {
+    ...stubs,
+    EpNotificationBar: true,
+  };
+
+  const emptyResultTrue = (wrapper: any) => {
+    expect(wrapper.html()).not.toContain('katselet-voimaantulevaa-perustetta');
+    expect(wrapper.html()).not.toContain('katselet-talla-hetkella-voimassaolevaa-perustetta');
+  };
+
+  describe('ilman muutosmaarayksia', () => {
+    const currentRoute = {
+      params: {
+        lang: 'fi',
+      },
+    };
+    const wrapper = mount(EpPerusteNotificationBar as any, {
+      localVue,
+      propsData: {
+        peruste,
+        julkaisut: [
+          {
+            revision: 1,
+          },
+          {
+            revision: 2,
+          },
+        ],
+      },
+      stubs: {
+        ...komponenttiStubit,
+      },
+      mocks: {
+        ...mocks,
+        $route: currentRoute,
+        $router: {
+          async push(route) {
+            currentRoute.params = route.params || {};
+            wrapper.vm.$forceUpdate();
+          },
+        },
+      },
+    });
+
+    test('current julkaisu', async () => {
+      emptyResultTrue(wrapper);
+
+      await wrapper.vm.$router.push({ params: { revision: '1' } });
+      await wrapper.vm.$nextTick();
+
+      emptyResultTrue(wrapper);
+    });
+  });
+
+  describe('muutosmaarays voimassa', () => {
+    const currentRoute = {
+      params: {
+        lang: 'fi',
+      },
+    };
+    const wrapper = mount(EpPerusteNotificationBar as any, {
+      localVue,
+      propsData: {
+        peruste,
+        julkaisut: [
+          {
+            revision: 1,
+          },
+          {
+            revision: 2,
+            muutosmaarays: {
+              voimassaoloAlkaa: datePlusDays(-1).getTime(),
+            },
+          },
+        ],
+      },
+      stubs: {
+        ...komponenttiStubit,
+      },
+      mocks: {
+        ...mocks,
+        $route: currentRoute,
+        $router: {
+          async push(route) {
+            currentRoute.params = route.params || {};
+            wrapper.vm.$forceUpdate();
+          },
+        },
+      },
+    });
+
+    test('current julkaisu', async () => {
+      emptyResultTrue(wrapper);
+
+      await wrapper.vm.$router.push({ params: { revision: '1' } });
+      await wrapper.vm.$nextTick();
+
+      emptyResultTrue(wrapper);
+    });
+  });
+
+  describe('muutosmaarays tulossa voimaan', () => {
+    const currentRoute = {
+      params: {
+        lang: 'fi',
+      },
+    };
+    const wrapper = mount(EpPerusteNotificationBar as any, {
+      localVue,
+      propsData: {
+        peruste,
+        julkaisut: [
+          {
+            revision: 1,
+          },
+          {
+            revision: 2,
+            muutosmaarays: {
+              voimassaoloAlkaa: datePlusDays(5).getTime(),
+            },
+          },
+        ],
+      },
+      stubs: {
+        ...komponenttiStubit,
+      },
+      mocks: {
+        ...mocks,
+        $route: currentRoute,
+        $router: {
+          async push(route) {
+            currentRoute.params = route.params || {};
+            wrapper.vm.$forceUpdate();
+          },
+        },
+      },
+    });
+
+    test('one julkaisu', async () => {
+      await wrapper.vm.$router.push({ params: { revision: '2' } });
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.html()).toContain('katselet-voimaantulevaa-perustetta');
+
+      await wrapper.vm.$router.push({ params: { revision: '1' } });
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.html()).toContain('katselet-talla-hetkella-voimassaolevaa-perustetta');
+    });
+  });
+
+  describe('muutosmaarays tulossa voimaan, selataan aiempaa', () => {
+    const currentRoute = {
+      params: {
+        lang: 'fi',
+      },
+    };
+    const wrapper = mount(EpPerusteNotificationBar as any, {
+      localVue,
+      propsData: {
+        peruste,
+        julkaisut: [
+          {
+            revision: 1,
+          },
+          {
+            revision: 2,
+          },
+          {
+            revision: 3,
+            muutosmaarays: {
+              voimassaoloAlkaa: datePlusDays(1).getTime(),
+            },
+          },
+        ],
+      },
+      stubs: {
+        ...komponenttiStubit,
+      },
+      mocks: {
+        ...mocks,
+        $route: currentRoute,
+        $router: {
+          async push(route) {
+            currentRoute.params = route.params || {};
+            wrapper.vm.$forceUpdate();
+          },
+        },
+      },
+    });
+
+    test('ensimmainen julkaisu', async () => {
+      await wrapper.vm.$router.push({ params: { revision: '3' } });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.html()).toContain('katselet-voimaantulevaa-perustetta');
+
+      await wrapper.vm.$router.push({ params: { revision: '2' } });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.html()).toContain('katselet-talla-hetkella-voimassaolevaa-perustetta');
+
+      await wrapper.vm.$router.push({ params: { revision: '1' } });
+      await wrapper.vm.$nextTick();
+
+      emptyResultTrue(wrapper);
+    });
+  });
+
+  describe('muutosmaarays voimassa ja tulossa voimaan, selataan aiempia', () => {
+    const currentRoute = {
+      params: {
+        lang: 'fi',
+      },
+    };
+
+    const wrapper = mount(EpPerusteNotificationBar as any, {
+      localVue,
+      propsData: {
+        peruste,
+        julkaisut: [
+          {
+            revision: 1,
+          },
+          {
+            revision: 2,
+            muutosmaarays: {
+              voimassaoloAlkaa: datePlusDays(-1).getTime(),
+            },
+          },
+          {
+            revision: 3,
+          },
+          {
+            revision: 4,
+          },
+          {
+            revision: 5,
+            muutosmaarays: {
+              voimassaoloAlkaa: datePlusDays(2).getTime(),
+            },
+          },
+          {
+            revision: 6,
+          },
+        ],
+      },
+      stubs: {
+        ...komponenttiStubit,
+      },
+      mocks: {
+        ...mocks,
+        $route: currentRoute,
+        $router: {
+          async push(route) {
+            currentRoute.params = route.params || {};
+            wrapper.vm.$forceUpdate();
+          },
+        },
+      },
+    });
+
+    test('muutosmaarays 6 julkaisua', async () => {
+      await wrapper.vm.$router.push({ params: { revision: '6' } });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.html()).toContain('katselet-voimaantulevaa-perustetta');
+
+      await wrapper.vm.$router.push({ params: { revision: '5' } });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.html()).toContain('katselet-voimaantulevaa-perustetta');
+
+      await wrapper.vm.$router.push({ params: { revision: '4' } });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.html()).toContain('katselet-talla-hetkella-voimassaolevaa-perustetta');
+
+      await wrapper.vm.$router.push({ params: { revision: '3' } });
+      await wrapper.vm.$nextTick();
+      emptyResultTrue(wrapper);
+
+      await wrapper.vm.$router.push({ params: { revision: '2' } });
+      await wrapper.vm.$nextTick();
+      emptyResultTrue(wrapper);
+
+      await wrapper.vm.$router.push({ params: { revision: '1' } });
+      await wrapper.vm.$nextTick();
+      emptyResultTrue(wrapper);
+    });
+  });
+});
---
+++ src/components/EpNotificationBar/EpPerusteNotificationBar.vue
@@ -0,0 +1,122 @@
+<template>
+  <EpNotificationBar :julkaisu-pvm="julkaisuPvm" :has-sisalto-kielelle="hasSisaltoKielelle">
+    <template v-if="voimassaolo">
+      <div class="notifikaatio-text" v-if="voimassaolo === 'tuleva'">
+        <span>{{ $t('katselet-voimaantulevaa-perustetta', {voimaantulo: $sd(currentJulkaisu.muutosmaarays.voimassaoloAlkaa)}) }} </span>
+        <span class="btn-link clickable korostus" @click="toVoimassaolevaanJulkaisuun">{{$t('voimassaolevaan-perusteeseen')}}.</span>
+      </div>
+      <div class="notifikaatio-text" v-if="voimassaolo === 'voimassa'">
+        <span>{{ $t('katselet-talla-hetkella-voimassaolevaa-perustetta') }}. </span>
+        <span>{{ $t('siirry') }} </span>
+        <span class="btn-link clickable korostus" @click="toUusimpaanJulkaisuun">{{$t('uusimpaan-perusteeseen')}}, </span>
+        <span>{{ $t('joka-on-tulossa-voimaan', {voimaantulo: $sd(uusinJulkaisu.muutosmaarays.voimassaoloAlkaa)}) }}.</span>
+      </div>
+    </template>
+  </EpNotificationBar>
+</template>
+
+<script lang="ts">
+import * as _ from 'lodash';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import EpNotificationBar from '@/components/EpNotificationBar/EpNotificationBar.vue';
+import { Kielet } from '@shared/stores/kieli';
+
+@Component({
+  components: {
+    EpNotificationBar,
+  },
+})
+export default class EpPerusteNotificationBar extends Vue {
+  @Prop()
+  private julkaisut?: any[];
+
+  @Prop()
+  private peruste?: any;
+
+  get julkaisutSorted() {
+    return _.sortBy(this.julkaisut, 'revision');
+  }
+
+  get julkaisutReversed() {
+    return _.clone(this.julkaisutSorted).reverse();
+  }
+
+  get ensimmainenTulevaMuutosmaarays() {
+    return _.find(this.julkaisutSorted, julkaisu => julkaisu.muutosmaarays && julkaisu.muutosmaarays.voimassaoloAlkaa > Date.now());
+  }
+
+  get uusinTulevaMuutosmaarays() {
+    return _.find(this.julkaisutReversed, julkaisu => julkaisu.muutosmaarays && julkaisu.muutosmaarays.voimassaoloAlkaa > Date.now());
+  }
+
+  get uusinVoimassaolevaJulkaisu() {
+    return _.find(this.julkaisutReversed, julkaisu => julkaisu.revision < this.ensimmainenTulevaMuutosmaarays.revision);
+  }
+
+  get voimassaolo() {
+    if (this.ensimmainenTulevaMuutosmaarays) {
+      if (this.currentRevision >= this.ensimmainenTulevaMuutosmaarays?.revision) {
+        return 'tuleva';
+      }
+
+      if (this.currentRevision >= this.uusinVoimassaolevaJulkaisu?.revision) {
+        return 'voimassa';
+      }
+    }
+  }
+
+  get currentRevision() {
+    return _.toNumber(this.$route?.params?.revision || _.max(_.map(this.julkaisut, 'revision')));
+  }
+
+  get uusinJulkaisu() {
+    return {
+      ..._.last(this.julkaisut),
+      muutosmaarays: this.uusinTulevaMuutosmaarays?.muutosmaarays,
+    };
+  }
+
+  get currentJulkaisu() {
+    const currentJulkaisu = _.find(this.julkaisut, julkaisu => julkaisu.revision === this.currentRevision);
+
+    if (currentJulkaisu) {
+      return {
+        ...currentJulkaisu,
+        muutosmaarays: currentJulkaisu?.muutosmaarays || _.find(this.julkaisutReversed, j => j.revision < currentJulkaisu.revision && j.muutosmaarays)?.muutosmaarays,
+      };
+    }
+  }
+
+  get julkaisuPvm() {
+    return this.currentJulkaisu?.luotu;
+  }
+
+  async toVoimassaolevaanJulkaisuun() {
+    let route = _.assign({}, this.$route);
+    await this.$router.push(
+      {
+        name: route.name!,
+        params: {
+          ...route.params,
+          revision: _.toString(this.uusinVoimassaolevaJulkaisu?.revision),
+        },
+      });
+  }
+
+  get hasSisaltoKielelle() {
+    return _.includes(this.peruste?.kielet, _.toString(Kielet.getSisaltoKieli.value));
+  }
+
+  async toUusimpaanJulkaisuun() {
+    let route = _.assign({}, this.$route);
+    delete route.params?.revision;
+    await this.$router.push({ name: route.name!, params: route.params });
+    this.$router.go(0);
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@shared/styles/_variables.scss';
+
+</style>
