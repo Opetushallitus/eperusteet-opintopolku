3ed582571533325939b1b5b7d65d22c423cda930
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-01-08 11:05:54 +0200

    EP-4379

--- eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/servlet/DigiOsaaminenServlet.java
+++ eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/servlet/DigiOsaaminenServlet.java
@@ -5,11 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
