092a89497ce530e26aecc4f423915519990182ce
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-01-08 11:05:54 +0200

    EP-4379

--- eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/EperusteetOpintopolkuApplication.java
+++ eperusteet-opintopolku-app/src/main/java/fi/vm/sade/eperusteet/opintopolku/EperusteetOpintopolkuApplication.java
@@ -2,6 +2,8 @@ package fi.vm.sade.eperusteet.opintopolku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
